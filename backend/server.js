25e382b17c993e0f75abd62bf55676e61a4e0b6c
muksis <60219890+muksis@users.noreply.github.com>
2022-06-03 13:36:55 -0400

    Add ticket model & routes

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,8 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
 
 app.use(errorHandler);
 
