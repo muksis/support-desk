6c5897f9a77cdd642cdb7b0f2afcffa327e0c596
muksis <60219890+muksis@users.noreply.github.com>
2022-06-06 20:07:23 -0400

    Add back button

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -4,10 +4,11 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { createTicket, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
 
 function NewTicket() {
   const { user } = useSelector((state) => state.auth);
-  const { isLoading, isError, isSuccess,message } = useSelector((state) => state.ticket);
+  const { isLoading, isError, isSuccess, message } = useSelector((state) => state.ticket);
 
   const [name] = useState(user.name);
   const [email] = useState(user.email);
@@ -32,7 +33,7 @@ function NewTicket() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    dispatch(createTicket(product, description));
+    dispatch(createTicket({ product, description }));
   };
 
   if (isLoading) {
@@ -41,6 +42,7 @@ function NewTicket() {
 
   return (
     <>
+      <BackButton url="/" />
       <section className="heading">
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
